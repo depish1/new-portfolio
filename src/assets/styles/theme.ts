1e5deb935018772351cd9cf54443067b13c3c8dc
depish1 <michal.depa@op.pl>
2021-10-01 21:55:14 +0200

    change color theme

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -2,29 +2,9 @@ export const theme = {
   colors: {
     error: "#ff1a1a",
     success: "#3e8914",
-    // section1: {
-    //   background: "#001524",
-    //   text: "#ffecd1",
-    //   primary: "#ff7d00",
-    // },
-    // section2: {
-    //   background: "#ff1654",
-    //   text: "#edf2f4",
-    //   primary: "#2b2d42",
-    // },
-    // section2: {
-    //   background: "#003566",
-    //   text: "#fff",
-    //   primary: "#ffc300",
-    // },
-    // section1: {
-    //   background: "#2b2d42",
-    //   text: "#edf2f4",
-    //   primary: "#ff1654",
-    // },
-    background: "#001524",
-    text: "#edf2f4",
-    primary: "#ff7d00",
+    background: "#171717",
+    text: "#EDEDED",
+    primary: "#DA0037",
   },
   headerHeight: "4.5rem",
   menuBreakPoint: "1000px",
